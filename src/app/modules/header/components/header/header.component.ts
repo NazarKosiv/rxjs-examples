326edab016284ad00fc70d05e997effb43d3085b
Nazar Kosiv <nazarkosiv94@gmail.com>
2019-04-14 01:11:36 +0300

    feature/hide-header-on-scroll

--- src/app/modules/header/components/header/header.component.ts
+++ src/app/modules/header/components/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { fromEvent, Observable, timer, Subscription } from 'rxjs';
+import { tap, switchMap, filter, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  hideHeader = false;
 
-  constructor() { }
+  private hideHeaderOnScrollSub: Subscription;
+
+  constructor() {}
 
   ngOnInit() {
+    const hideHeaderOnScroll$: Observable<MouseEvent> = fromEvent<MouseEvent>(window, 'scroll');
+
+    this.hideHeaderOnScrollSub = hideHeaderOnScroll$
+      .pipe(
+        tap(() => (this.hideHeader = true)),
+        debounceTime(1000),
+        tap(() => (this.hideHeader = false))
+      )
+      .subscribe();
   }
 
+  ngOnDestroy() {
+    this.hideHeaderOnScrollSub.unsubscribe();
+  }
 }
